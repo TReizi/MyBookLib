feb546c616d2a21ff28c073111626602b8a01ec5
[Artem Lutsenko] <treiziyes@gmail.com>
2022-09-15 08:41:24 +0300

    Upgrade your previous implementation (level 2) by adding a relational database to your project - PostgreSQL. And implement several new features to add more complexity to the business logic. Recommendation: start thinking about multi-layered architecture. Probably, you want to move all your business logic from main class and main method to a “service” class. Required knowledge • Java Core • JDBC Main features • List all readers with their borrowed books, if no books are currently borrowed – print “(no books borrowed)” • List all books with their borrowers. If the book is not borrowed, print “(available)”

--- src/main/java/com/booklibrary/connectionSettings/ConnectionSettingsData.java
+++ src/main/java/com/booklibrary/connectionSettings/ConnectionSettingsData.java
@@ -1,10 +1,7 @@
 package com.booklibrary.connectionSettings;
 
 public class ConnectionSettingsData {
-    public  final String DB_USERNAME = "root";
-    public  final String DB_PASSWORD = "Artem0906";
-    public  final String DB_URL = "jdbc:mysql://127.0.0.1:3306/my_db";
-
-
-
+  public final String DB_USERNAME = "bestuser";
+  public final String DB_PASSWORD = "bestuser";
+  public final String DB_URL = "jdbc:mysql://127.0.0.1:3306/my_db";
 }
